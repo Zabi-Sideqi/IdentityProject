4e34e1bc55dc36e47da328e13812a06c5962646b
Zabi Sideqi <zabi.sideqi@gmail.com>
2026-07-16 18:24:44 +0200

    Fixed the Issues added by ScaffoldedIdentity

--- IdentityProject/Data/ApplicationDbContext.cs
+++ IdentityProject/Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IdentityProject.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
     }
 }
